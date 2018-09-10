084f928b896482e96c3f82c5383e30001670c22c
mark <1101112378@qq.com>
2018-09-10 16:49:37 +0800

    登录验证

--- appApi/user.js
+++ appApi/user.js
@@ -10,7 +10,7 @@ router.get('/', async(ctx) => {
     ctx.body = "<h2>我是首页</h2>"
 })
 
-router.post('/login', async(ctx) => {
+router.post('/reg', async(ctx) => {
 
     const User = mongoose.model('User');
 
@@ -29,6 +29,45 @@ router.post('/login', async(ctx) => {
             message: error
         }
     })
+})
+
+router.post('/login', async(ctx) => {
+
+    let loginUser = ctx.request.body;
+    console.log(loginUser);
+    let userName = loginUser.userName;
+    let password = loginUser.password;
+
+    // 引入User的model
+    const User = mongoose.model('User');
+
+    await User.findOne({ userName: userName }).exec().then(async(res) => {
+        console.log(res);
+        if (res) {
+            let newUser = new User()
+            await newUser.comparePassword(password, res.password)
+                .then((isMatch) => {
+                    if (isMatch) {
+                        ctx.body = { code: 200, status: isMatch, message: '登陆成功' }
+                    } else {
+                        ctx.body = { code: 200, status: isMatch, message: '密码错误,请重新输入' }
+                    }
+                }).catch((err) => {
+                    console.log(err);
+                    ctx.body = { code: 500, status: false, message: err }
+                })
+        } else {
+            ctx.body = {
+                code: 200,
+                status: false,
+                message: "用户名不存在"
+            }
+        }
+    }).catch((err) => {
+        console.log(err);
+        ctx.body = { code: 500, message: err }
+    })
+
 })
 
 module.exports = router;
--- dataBase/schema/User.js
+++ dataBase/schema/User.js
@@ -15,10 +15,14 @@ const userSchema = new Schema({
     password: String,
     createAt: { type: Date, default: Date.now() },
     lastLoginAt: { type: Date, default: Date.now() }
+}, {
+    collection: 'user'
 })
 
 userSchema.pre('save', function(next) {
+
     console.log(this)
+
     bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
         if (err) {
             return next(err)
@@ -32,6 +36,22 @@ userSchema.pre('save', function(next) {
         })
     })
 })
+
+
+// 比对密码  
+userSchema.methods = {
+    comparePassword: (_password, password) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(_password, password, (err, isMatch) => {
+                if (!err) {
+                    resolve(isMatch)
+                } else {
+                    reject(err)
+                }
+            })
+        })
+    }
+}
 
 
 //发布模型
