8b3d6ebff6ede460f8080e6390a41667f9c6ca4e
mark <1101112378@qq.com>
2018-09-12 16:27:17 +0800

    类别列表 接口增加

--- appApi/goods.js
+++ appApi/goods.js
@@ -81,21 +81,75 @@ router.get('/insertAllCategorySubInfo', async(ctx) => {
     ctx.body = '数据开始导入'
 })
 
-// 查找接口
+// 商品详情接口
 router.post('/getDetailGoodsInfo', async(ctx) => {
-    let goodsId = ctx.request.body.goodsId // 接受到的参数id
-    const Goods = mongoose.model('Goods');
-    await Goods.findOne({ ID: goodsId }).exec()
-        .then(async(result) => {
-            if (result) {
-                ctx.body = { code: 200, status: true, data: result }
-            } else {
-                ctx.body = { code: 200, status: true, data: {}, message: '没有此产品' }
-            }
-        }).catch((err) => {
-            console.log(err);
-            ctx.body = { code: 500, message: err }
-        })
+    try {
+        let goodsId = ctx.request.body.goodsId // 接受到的参数id
+        const Goods = mongoose.model('Goods');
+        let result = await Goods.findOne({ ID: goodsId }).exec()
+        if (result) {
+            ctx.body = { code: 200, status: true, data: result }
+        } else {
+            ctx.body = { code: 200, status: false, data: {}, message: '没有此产品' }
+        }
+    } catch (err) {
+        console.log(err);
+        ctx.body = { code: 500, message: err }
+    }
 })
+
+
+// 商品大类接口
+router.get('/getCategoryList', async(ctx) => {
+    try {
+        const Category = mongoose.model('Category');
+        let result = await Category.find().exec()
+        ctx.body = { code: 200, status: true, data: result }
+    } catch (err) {
+        console.log(err)
+        ctx.body = { code: 500, message: err }
+    }
+})
+
+
+// 商品小类接口
+router.post('/getCategorySubList', async(ctx) => {
+    try {
+        let categoryId = ctx.request.body.categoryId
+            // let categoryId = 1; // 后期替换为别的类型
+        const Category = mongoose.model('CategorySub')
+        let result = await Category.find({ MALL_CATEGORY_ID: categoryId }).exec()
+        if (result) {
+            ctx.body = { code: 200, status: true, data: result }
+        } else {
+            ctx.body = { code: 200, status: false, data: [], message: '没有数据信息' }
+        }
+    } catch (err) {
+        console.log(err);
+        ctx.body = { code: 500, message: err }
+    }
+
+
+})
+
+
+// 商品列表接口
+
+router.get('/getGoodsListByCategorySubID', async(ctx) => {
+    try {
+        let categorySubId = '2c9f6c946016ea9b016016f79c8e0000';
+        const Goods = mongoose.model('Goods');
+        let result = await Goods.find({ SUB_ID: categorySubId }).exec()
+        if (result) {
+            ctx.body = { code: 200, status: true, data: result }
+        } else {
+            ctx.body = { code: 200, status: false, data: [], message: '没有数据信息' }
+        }
+    } catch (err) {
+        console.log(err)
+        ctx.body = { code: 500, message: err }
+    }
+})
+
 
 module.exports = router;
